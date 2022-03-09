3b50b40e84658a266199fb794f73974ee8289319
aleks <aleks@DESKTOP-6JHRROB>
2022-03-09 23:36:45 +0100

    Add project files.

---
+++ DeskBooker.Core.Tests/Processor/DeskBookingRequestProcessorTests.cs
@@ -0,0 +1,45 @@
+﻿using DeskBooker.Core.Domain;
+using DeskBooker.Core.Processor;
+using System;
+using Xunit;
+
+namespace DeskBooker.Core.Tests.Processor
+{
+    public class DeskBookingRequestProcessorTests
+    {
+        private readonly DeskBookingRequestProcessor _processor;
+
+        public DeskBookingRequestProcessorTests()
+        {
+            _processor = new DeskBookingRequestProcessor();
+        }
+
+        [Fact]
+        public void ShouldReturnDeskBookingResultWithRequestValues()
+        {
+            var request = new DeskBookingRequest
+            {
+                FirstName = "Thomas",
+                LastName = "Huber",
+                Email = "brale@gmail.com",
+                Date = new DateTime(2022, 3, 9)
+            };
+
+            DeskBookingResult result = _processor.BookDesk(request);
+
+            Assert.NotNull(result);
+            Assert.Equal(request.FirstName, result.FirstName);
+            Assert.Equal(request.LastName, result.LastName);
+            Assert.Equal(request.Email, result.Email);
+            Assert.Equal(request.Date, result.Date);
+        }
+
+        [Fact]
+        public void ShouldThrowExceptionIfRequestIsNull()
+        {
+           var exception = Assert.Throws<ArgumentNullException>(() => _processor.BookDesk(null));
+
+            Assert.Equal("request", exception.ParamName);
+        }
+    }
+}
